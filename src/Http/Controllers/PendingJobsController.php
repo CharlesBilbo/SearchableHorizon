e7dd847c5ddbcb42086d34cd74e8cd82fd2dadf5
Charles Bilbo <charlesbilbo@personal>
2024-03-17 21:40:01 -0500

    add the ability to search pending

--- src/Http/Controllers/PendingJobsController.php
+++ src/Http/Controllers/PendingJobsController.php
@@ -3,8 +3,23 @@
 namespace SearchableHorizon\Http\Controller;
 
 use Laravel\Horizon\Http\Controllers\PendingJobsController as BaseController;
+use Illuminate\Http\Request;
 
 class PendingJobsController extends BaseController
 {
+    public function index(Request $request)
+    {
+        $jobs = $this->jobs->getPending(
+            $request->query('starting_at', -1),
+            $request->query('search')
+        )->map(function ($job) {
+            $job->payload = json_decode($job->payload);
+            return $job;
+        })->values();
 
+        return [
+            'jobs' => $jobs,
+            'total' => $this->jobs->countPending(),
+        ];
+    }
 }
